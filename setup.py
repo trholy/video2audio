e8336360ae3d77060674c4f8d53ccfb77bbe6085
Pablo <th0ly96@gmail.com>
2025-09-04 18:33:12 +0200

    Updated setup.

--- setup.py
+++ setup.py
@@ -3,20 +3,27 @@ from setuptools import setup, find_packages
 setup(
     name="video2audio",
     version="0.1.0",
-    description="Convert videos (any format) to audio (any format, e.g., MP3, WAV, AAC) using FFmpeg",
+    description="Convert videos to audio",
     author="Thomas R. Holy",
     python_requires=">=3.10",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    include_package_data=True,  # <- important
+    package_data={
+        "video2audio": [
+            "templates/*",    # include templates in package
+            "static/**/*",    # include all static files (css/js)
+        ],
+    },
     install_requires=[
-        "dash>=2.0.0",
         "flask>=2.0.0",
-        "dash-bootstrap-components"
+        "dash>=2.0.0",
+        "dash-bootstrap-components",
     ],
     entry_points={
         "console_scripts": [
-            "video2audio=video2audio.cli:main",  # CLI entry point
-            "video2audio-web=video2audio.webapp:run",  # Web UI entry point
+            "video2audio=video2audio.cli:main",
+            "video2audio-web=video2audio.webapp:run",
         ],
     },
 )
